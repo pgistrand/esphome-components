dce686ec4989f53d2f2cd910fa48bceef9eed32a
pgistrand <pgistrand@hotmail.com>
2025-10-29 14:46:26 +0100

    Update air_conditioner.h

--- components/midea/air_conditioner.h
+++ components/midea/air_conditioner.h
@@ -44,6 +44,8 @@ class AirConditioner : public ApplianceBase<dudanov::midea::ac::AirConditioner>,
   void set_odFVal_sensor(Sensor *sensor) { this->odFVal_sensor_ = sensor; }
   void on_status_change() override;
 
+  bool getDefrost(){return (bool)this->base_.getDefrost();}
+
   /* ############### */
   /* ### ACTIONS ### */
   /* ############### */
